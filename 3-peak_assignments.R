b8ee01a06444df43b0b11a7bf2d30fd1ef2ac191
Kaizad <kaizad.patel@pnnl.gov>
2020-04-10 09:46:40 -0700

    Update 3-peak_assignments.R

--- 3-peak_assignments.R
+++ 3-peak_assignments.R
@@ -9,15 +9,31 @@ assg_points =
 
 peaks_match = 
   peaks %>% 
+  dplyr::select(Core, ppm) %>% 
   left_join(assg_points, by = c("ppm" = "deltaH_ppm")) %>% 
+  #dplyr::mutate(matched = if_else(!is.na(moiety),"matched", "not matched"))
+  filter(!is.na(moiety)) %>% 
   dplyr::mutate(matched = if_else(!is.na(moiety),"matched", "not matched"))
+
 
 ggplot(peaks_match, aes(x = ppm, y = matched, color = abbrev))+
   geom_point()+
+  geom_point(data = peaks_match2, aes(x = ppm, y = matched, color = abbrev), shape = 2 )+
   facet_wrap(~Core)
 
 
 peaks_match2 = 
-  peaks %>% 
-  left_join(assg_points, by = c("ppm" = "deltaH_ppm")) %>% 
+  subset(merge(dplyr::select(peaks,Core,ppm), peak_assg), deltaH_start <= ppm & ppm <= deltaH_stop) %>% 
+  dplyr::select(-deltaH_ppm) %>% 
   dplyr::mutate(matched = if_else(!is.na(moiety),"matched", "not matched"))
+
+matched = rbind(peaks_match, peaks_match2)
+
+matched2 = 
+  peaks %>% 
+  left_join(matched, by = c("Core","ppm"))
+
+ggplot(matched2, aes(x = ppm, y = matched, color = abbrev))+
+  geom_point()+
+  scale_x_reverse()+
+  facet_wrap(~Core)
