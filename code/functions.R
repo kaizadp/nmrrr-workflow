f977a43165098b0439a289b5b99a7bad15d24d23
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2021-03-19 03:02:24 -0400

    Comment tweaks

--- code/functions.R
+++ code/functions.R
@@ -5,13 +5,13 @@
 rawdat <- read.csv("data/peaks/27.csv", stringsAsFactors = FALSE, check.names = FALSE)
 
 # The raw NMR data are in groups of nine columns, with the first column in the group
-# (the observation) number having no name. Start by finding these no-name columns and 
+# (the observation number) having no name. Start by finding these no-name columns and 
 # verifying they're exactly 9 positions part; otherwise we have a problem
 noname_cols <- which(names(rawdat) == "")
 if(!all(diff(noname_cols) == 9)) {
   stop("Formatting problem: data don't appear to be in 9-column groups")
 }
-names(rawdat)[noname_cols] <- "Obs"
+names(rawdat)[noname_cols] <- "Obs"  # give them a name
 
 # Extract each group in turn and store temporarily in a list
 nmr_list <- list()
@@ -20,4 +20,5 @@ for(nnc in seq_along(noname_cols)) {
 }
 
 # Finally, bind everything into a single data frame
-nmr_dat <- dplyr::bind_rows(nmr_list)   # could also use do.call("rbind", nmr_list)
+# This uses dplyr but we could also use base R: do.call("rbind", nmr_list)
+nmr_dat <- dplyr::bind_rows(nmr_list)
